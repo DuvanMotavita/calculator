4335662ee755d87c27f5ad96bb149b2b3b76077c
DuvanMotavita <duvanfel1@yahoo.com.co>
2024-11-25 18:32:11 -0500

    first commit

---
+++ src/app/calculator/components/Calculator/Calculator.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+
+@Component({
+  selector: 'calculator',
+  standalone: true,
+  imports: [CommonModule, CalculatorButtonComponent],
+  templateUrl: './Calculator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  // styles: `.is-command {
+  //   @apply bg-indigo-700 bg-opacity-20;
+  // }
+  // `,
+})
+export class CalculatorComponent {}

8f8227c4a2caaf24fa67a8412191e45b78f119e9
DuvanMotavita <duvanfel1@yahoo.com.co>
2024-12-23 16:03:30 -0500

    Added calculator service

--- src/app/calculator/components/Calculator/Calculator.component.ts
+++ src/app/calculator/components/Calculator/Calculator.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, viewChildren } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  viewChildren,
+} from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
@@ -8,34 +15,41 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   imports: [CommonModule, CalculatorButtonComponent],
   templateUrl: './Calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host:{
-    '(document:keyup)': 'handleKeyBoardEvent($event)'
-  }
+  host: {
+    '(document:keyup)': 'handleKeyBoardEvent($event)',
+  },
   // styles: `.is-command {
   //   @apply bg-indigo-700 bg-opacity-20;
   // }
   // `,
 })
 export class CalculatorComponent {
-  public calculatorButtons  = viewChildren(CalculatorButtonComponent)
-  handleClick(key:string){
-    console.log({key});
+  private calculatorService = inject(CalculatorService);
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+  public resultText = computed(() => this.calculatorService.resultText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
+  // get resultText(){
+  //   return this.calculatorService.resultText();
+  // }
+  handleClick(key: string) {
+    // console.log({ key });
+    this.calculatorService.constructNumber(key);
   }
   // @HostListener('document:keyup',['$event'])
-  handleKeyBoardEvent(event:KeyboardEvent){
-    const keyEquivalents:Record<string,string>  = {
-      Escape:'C',
+  handleKeyBoardEvent(event: KeyboardEvent) {
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
       Clear: 'C',
       '*': 'x',
       '/': '÷',
-      Enter: '='
-      
-    }
+      Enter: '=',
+    };
     const key = event.key;
     const keyValue = keyEquivalents[key] ?? key;
     this.handleClick(keyValue);
-    this.calculatorButtons().forEach(button =>{
-      button.keyboardPressedStyle(keyValue)
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyle(keyValue);
     });
   }
 }
---
+++ src/app/calculator/services/calculator.service.ts
@@ -0,0 +1,137 @@
+import { Injectable, signal } from '@angular/core';
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const operators = ['+', '-', '*', '/', '÷', 'x'];
+const specialOperators = ['+/-', '%', '.', '=', 'C', 'Backspace'];
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CalculatorService {
+  public resultText = signal('0');
+  public subResultText = signal('0');
+  public lastOperator = signal('+');
+
+  constructor() {}
+
+  public constructNumber(value: string): void {
+    if (![...numbers, ...operators, ...specialOperators].includes(value)) {
+      console.log('Invalid input', value);
+      return;
+    }
+    if (value === '=') {
+      this.calculateResult();
+      console.log('Calculate result');
+      return;
+    }
+    //Clear results
+    if (value === 'C') {
+      this.resultText.set('0');
+      this.subResultText.set('0');
+      this.lastOperator.set('+');
+      return;
+    }
+    //Backspace
+    //Todo: Review when we have a negative number
+    if (value === 'Backspace') {
+      if (this.resultText() === '0') return;
+      // if (this.resultText() === '-0') {
+      //   this.resultText.set('0');
+      //   return;
+      // }
+      if (this.resultText().includes('-') && this.resultText().length === 2) {
+        this.resultText.set('0');
+        return;
+      }
+      if (this.resultText().length === 1) {
+        this.resultText.set('0');
+        return;
+      }
+      this.resultText.update((v) => v.slice(0, -1));
+      return;
+    }
+    //Operator aplication
+    if (operators.includes(value)) {
+      // this.calculateResult();
+      this.lastOperator.set(value);
+      this.subResultText.set(this.resultText());
+      this.resultText.set('0');
+      return;
+    }
+
+    //Character limit handler
+    if (this.resultText().length >= 10) {
+      console.log('Max length reached');
+      return;
+    }
+    //validate decimal dot
+
+    if (value === '.' && !this.resultText().includes('.')) {
+      if (this.resultText() === '0' || this.resultText() === '') {
+        this.resultText.set('0.');
+        return;
+      }
+      this.resultText.update((text) => text + '.');
+      return;
+    }
+    //Zero handler
+    if (
+      value === '0' &&
+      (this.resultText() === '0' || this.resultText() === '-0')
+    ) {
+      return;
+    }
+    // Change operator
+    if (value === '+/-') {
+      if (this.resultText().includes('-')) {
+        this.resultText.update((text) => text.slice(1));
+        return;
+      }
+      this.resultText.update((text) => '-' + text);
+      return;
+    }
+    //Numbers
+    if (numbers.includes(value)) {
+      if (this.resultText() === '0') {
+        this.resultText.set(value);
+        return;
+      }
+      if (this.resultText() === '-0') {
+        this.resultText.set('-' + value);
+        return;
+      }
+    }
+    this.resultText.update((text) => text + value);
+  }
+
+  public calculateResult(): void {
+    const number1 = parseFloat(this.subResultText());
+    const number2 = parseFloat(this.resultText());
+    let result = 0;
+    switch (this.lastOperator()) {
+      case '+':
+        result = number1 + number2;
+        break;
+      case '-':
+        result = number1 - number2;
+        break;
+      case '*':
+        result = number1 * number2;
+        break;
+      case 'x':
+        result = number1 * number2;
+        break;
+      case '/':
+        result = number1 / number2;
+        break;
+      case '÷':
+        result = number1 / number2;
+        break;
+      default:
+        result = 0;
+        break;
+    }
+    this.resultText.set(result.toString());
+    this.subResultText.set('0');
+  }
+}
